321c0ea413f65f49002901ede750814106e25098
Ash <hrksingh17@gmail.com>
2025-01-03 19:56:35 +0530

    starting JS

---
+++ foundationalJS/script.js
@@ -0,0 +1,15 @@
+let first_name = "John";
+let last_name = "Doe";
+let age = 25;
+
+console.log(first_name + " " + last_name + " is " + age + " years old.");
+
+const pi = 3.14159;
+console.log(pi);
+
+console.log(3 + 2) - 76 * (1 + 1);
+var a = 15; // var is a global variable it allows you to reassign the value
+console.log(a);
+
+let embString = `I am embedded string`;
+console.log(`This is example of embedded string ${embString}`);
